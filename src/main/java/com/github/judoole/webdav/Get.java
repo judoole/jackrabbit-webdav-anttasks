6310ac0969eb9ad9fb66cb0cfcf1954b7abc6932
Ole Christian LangfjÃ¦ran <judoole@gmail.com>
2012-08-23 09:55:59 +0200

    Working put and delete

---
+++ src/main/java/com/github/judoole/webdav/Get.java
@@ -0,0 +1,87 @@
+package com.github.judoole.webdav;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Get extends Command {
+        private String url;
+        private String file;
+        private String outFile;
+        private boolean verbose = false;
+        private boolean overwrite = false;
+        
+        public void setUrl(String url) {
+                this.url = url;
+        }
+        
+        public void setFile(String file) {
+                this.file = file;
+        }
+
+        public void setOutFile(String outFile) {
+                this.outFile = outFile;
+        }
+        
+        public void setVerbose(boolean verbose) {
+                this.verbose = verbose;
+        }
+        
+        public void setOverwrite(boolean overwrite) {
+                this.overwrite = overwrite;
+        }
+
+        public void execute() {
+                try {
+                HttpClient client = new HttpClient();
+                Credentials creds = new UsernamePasswordCredentials(user, password);
+                client.getState().setCredentials(AuthScope.ANY, creds);
+                File f = new File(outFile);
+                long startTime = System.currentTimeMillis();
+                
+                if (this.overwrite || !f.exists()) {
+                        if (verbose) {
+                                if (f.exists()) log("Overwriting " + f.getAbsolutePath());
+                                        log("Downloading " + url + "/" + file + " to " + f.getAbsolutePath());
+                        }
+                        
+                        //Download the file
+                        GetMethod method = new GetMethod(url + "/" + file);
+                        client.executeMethod(method);
+                        
+                        //200 OK => No issues
+                        if (method.getStatusCode() != 200)
+                                throw new Exception(method.getStatusCode() + " " + method.getStatusText());
+                        
+                        InputStream is = method.getResponseBodyAsStream();
+                        OutputStream out = new FileOutputStream(f);
+                        byte buf[] = new byte[1024];
+                        int len;
+                        
+                        while((len = is.read(buf))>0) {
+                            out.write(buf,0,len);
+                                out.flush();
+                        }
+                        out.close();
+                        
+                        if (verbose) {
+                                long endTime = System.currentTimeMillis();
+                                long elapsed = ((endTime - startTime) / 1000);
+                                log (file + " took " + elapsed + " seconds to complete");
+                        }
+                } else
+                        log ("Skipping " + file);
+
+        } catch (Exception e) {
+                e.printStackTrace();
+            } 
+        }
+
+}
---
+++ src/main/java/com/github/judoole/webdav/JackrabbitWebdavTasks.java
@@ -0,0 +1,52 @@
+package com.github.judoole.webdav;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JackrabbitWebdavTasks extends Task {
+    private List<Command> commands = new ArrayList<Command>();
+    private String username = null;
+    private String password = null;
+
+    public void addPut(Put put) {
+        put.setPassword(password);
+        put.setUser(username);
+        this.commands.add(put);
+    }
+
+    public void addDelete(Delete delete) {
+        delete.setPassword(password);
+        delete.setUser(username);
+        this.commands.add(delete);
+    }
+
+    public void addGet(Get get) {
+        get.setPassword(password);
+        get.setUser(username);
+        this.commands.add(get);
+    }
+
+    @Override
+    public void execute() {
+        try {
+            for (Command command : this.commands) {
+                command.execute();
+            }
+        } catch (Exception e) {
+            throw new BuildException(e);
+        }
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
+
